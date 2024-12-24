fd88e6a7c7e6f965a154a37a274a69fdf63dde13
Vinayak Prakash <vinayakprakash2121@gmail.com>
2024-12-24 13:38:13 +0530

    d

--- alu_decoder.v
+++ alu_decoder.v
@@ -11,27 +11,19 @@ always @(*) begin
     case (ALUOp)
         2'b00: ALUControl = 3'b000;             // addition
         2'b01: ALUControl = 3'b001;             // subtraction
-        default:
             case (funct3) // R-type or I-type ALU
                 3'b000: begin
-                    // True for R-type subtract
+                    // R-type subtract
                     if   (funct7b5 & opb5) ALUControl = 3'b001; //sub
-                    else ALUControl = 3'b000; // add, addi
+                    else                   ALUControl = 3'b000; // add
                 end
                 3'b001:  ALUControl = 3'b100;//sll, slli
-					 
                 3'b010:  ALUControl = 3'b101; // slt, slti
-					 
-					 3'b011:  ALUControl = 3'b101; //sltu ,sltiu
-					 
-                3'b100:  ALUControl = 3'b111;//xor, xori
-					 
-					 3'b101:  ALUControl = 3'b110; //srl, srli ,sra,srai
-					 
-                3'b110:  ALUControl = 3'b011; // or, ori
-					 
+				3'b011:  ALUControl = 3'b101; //sltu ,sltiu	 
+                3'b100:  ALUControl = 3'b111;//xor, xori	 
+				3'b101:  ALUControl = 3'b110; //srl, srli ,sra,srai	 
+                3'b110:  ALUControl = 3'b011; // or, ori	 
                 3'b111:  ALUControl = 3'b010; // and, andi
-                
                 default: ALUControl = 3'bxxx; // ???
             endcase
     endcase
